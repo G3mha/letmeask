7b9e86338eb686559c81c7a02f5df194f77a1f24
Enricco Gemha <riccogemha@gmail.com>
2022-05-11 20:35:27 -0300

    NewRoom on

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Home } from './pages/Home'
-import { BrowserRouter, Route } from 'react-router-dom'
+// import { Home } from './pages/Home'
+import { BrowserRouter } from 'react-router-dom'
+import { NewRoom } from './pages/NewRoom';
 
 function App() {
   return (
     <BrowserRouter>
-        <Home />
+        <NewRoom />
     </BrowserRouter>
   );
 }
